d6d1cc92608044d9557c7fbe38edbb73d11a978a
Lữ Thanh Vĩnh <luthanhvinh@dtu.edu.vn>
2016-03-30 20:26:02 +0700

    Điều chỉnh menu color

--- app/src/main/java/com/example/applycation/calculator/activities/BasicCalculator.java
+++ app/src/main/java/com/example/applycation/calculator/activities/BasicCalculator.java
@@ -42,7 +42,7 @@ public class BasicCalculator extends BaseActivity implements View.OnClickListene
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.calculator_basic);
 
         attachIdToView();
         attachOnClickListener();
